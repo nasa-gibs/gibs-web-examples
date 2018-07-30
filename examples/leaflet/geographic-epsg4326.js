a5ac973acca5e3608fb91f341c1ef3901e79f308
Mike McGann <mike.mcgann@nasa.gov>
2018-07-30 15:48:01 -0400

    update to leaflet 1.3.3, update proj4leaflet-1.0.1, fix epsg:4326 leaflet example

--- examples/leaflet/geographic-epsg4326.js
+++ examples/leaflet/geographic-epsg4326.js
@@ -18,67 +18,87 @@
  * limitations under the License.
  */
 
-window.onload = function() {
+window.onload = function () {
 
-    var EPSG4326 = new L.Proj.CRS(
-        "EPSG:4326",
-        "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs", {
-            origin: [-180, 90],
-            resolutions: [
-                0.5625,
-                0.28125,
-                0.140625,
-                0.0703125,
-                0.03515625,
-                0.017578125,
-                0.0087890625,
-                0.00439453125,
-                0.002197265625
-            ],
-            // Values are x and y here instead of lat and long elsewhere.
-            bounds: [
-               [-180, -90],
-               [180, 90]
-            ]
-        }
-    );
+  var EPSG4326 = new L.Proj.CRS(
+    "EPSG:4326",
+    "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs", {
+      origin: [-180, 90],
+      resolutions: [
+        0.5625,
+        0.28125,
+        0.140625,
+        0.0703125,
+        0.03515625,
+        0.017578125,
+        0.0087890625,
+        0.00439453125,
+        0.002197265625
+      ],
+      // Values are x and y here instead of lat and long elsewhere.
+      bounds: L.Bounds([
+        [-180, -90],
+        [180, 90]
+      ])
+    }
+  );
 
-    var map = L.map("map", {
-        center: [0, 0],
-        zoom: 2,
-        maxZoom: 8,
-        crs: EPSG4326,
-        maxBounds: [
-            [-120, -220],
-            [120, 220]
-        ]
-    });
+  var map = L.map("map", {
+    center: [0, 0],
+    zoom: 2,
+    maxZoom: 8,
+    crs: EPSG4326,
+    maxBounds: [
+      [-120, -220],
+      [120, 220]
+    ]
+  });
 
-    var template =
-        "//map1{s}.vis.earthdata.nasa.gov/wmts-geo/" +
-        "{layer}/default/{time}/{tileMatrixSet}/{z}/{y}/{x}.jpg";
+  var template =
+    "//gibs-{s}.earthdata.nasa.gov/wmts/epsg4326/best/" +
+    "{layer}/default/{time}/{tileMatrixSet}/{z}/{y}/{x}.jpg";
 
-    var layer = L.tileLayer(template, {
-        layer: "MODIS_Terra_CorrectedReflectance_TrueColor",
-        tileMatrixSet: "EPSG4326_250m",
-        time: "2013-11-04",
-        tileSize: 512,
-        subdomains: "abc",
-        noWrap: true,
-        continuousWorld: true,
-        // Prevent Leaflet from retrieving non-existent tiles on the
-        // borders.
-        bounds: [
-            [-89.9999, -179.9999],
-            [89.9999, 179.9999]
-        ],
-        attribution:
-            "<a href='https://wiki.earthdata.nasa.gov/display/GIBS'>" +
-            "NASA EOSDIS GIBS</a>&nbsp;&nbsp;&nbsp;" +
-            "<a href='https://github.com/nasa-gibs/web-examples/blob/release/examples/leaflet/geographic-epsg4326.js'>" +
-            "View Source" +
-            "</a>"
-    });
+  var layer = L.tileLayer(template, {
+    layer: "MODIS_Terra_CorrectedReflectance_TrueColor",
+    tileMatrixSet: "EPSG4326_250m",
+    time: "2013-11-04",
+    tileSize: 512,
+    subdomains: "abc",
+    noWrap: true,
+    continuousWorld: true,
+    // Prevent Leaflet from retrieving non-existent tiles on the
+    // borders.
+    bounds: [
+      [-89.9999, -179.9999],
+      [89.9999, 179.9999]
+    ],
+    attribution:
+      "<a href='https://wiki.earthdata.nasa.gov/display/GIBS'>" +
+      "NASA EOSDIS GIBS</a>&nbsp;&nbsp;&nbsp;" +
+      "<a href='https://github.com/nasa-gibs/web-examples/blob/release/examples/leaflet/geographic-epsg4326.js'>" +
+      "View Source" +
+      "</a>"
+  });
 
-    map.addLayer(layer);
+  map.addLayer(layer);
 };
+
+ /*
+ * Workaround for 1px lines appearing in some browsers due to fractional transforms
+ * and resulting anti-aliasing.
+ * https://github.com/Leaflet/Leaflet/issues/3575
+ */
+(function(){
+  var originalInitTile = L.GridLayer.prototype._initTile
+  L.GridLayer.include({
+      _initTile: function (tile) {
+          originalInitTile.call(this, tile);
+
+          var tileSize = this.getTileSize();
+
+          tile.style.width = tileSize.x + 1 + 'px';
+          tile.style.height = tileSize.y + 1 + 'px';
+      }
+  });
+})()
+
