78bfdafe0f6f4ff10cd837454710cf9ac7d74cdc
Mike McGann <mike.mcgann@nasa.gov>
2018-07-31 12:18:41 -0400

    update all leaflet examples, add linter

--- examples/leaflet/geographic-epsg4326.js
+++ examples/leaflet/geographic-epsg4326.js
@@ -5,24 +5,23 @@
  * Administrator of the National Aeronautics and Space Administration.
  * All Rights Reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the 'License');
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an 'AS IS' BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 
 window.onload = function () {
-
   var EPSG4326 = new L.Proj.CRS(
-    "EPSG:4326",
-    "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs", {
+    'EPSG:4326',
+    '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs', {
       origin: [-180, 90],
       resolutions: [
         0.5625,
@@ -43,7 +42,7 @@ window.onload = function () {
     }
   );
 
-  var map = L.map("map", {
+  var map = L.map('map', {
     center: [0, 0],
     zoom: 2,
     maxZoom: 8,
@@ -55,15 +54,15 @@ window.onload = function () {
   });
 
   var template =
-    "//gibs-{s}.earthdata.nasa.gov/wmts/epsg4326/best/" +
-    "{layer}/default/{time}/{tileMatrixSet}/{z}/{y}/{x}.jpg";
+    '//gibs-{s}.earthdata.nasa.gov/wmts/epsg4326/best/' +
+    '{layer}/default/{time}/{tileMatrixSet}/{z}/{y}/{x}.jpg';
 
   var layer = L.tileLayer(template, {
-    layer: "MODIS_Terra_CorrectedReflectance_TrueColor",
-    tileMatrixSet: "EPSG4326_250m",
-    time: "2013-11-04",
+    layer: 'MODIS_Terra_CorrectedReflectance_TrueColor',
+    tileMatrixSet: 'EPSG4326_250m',
+    time: '2013-11-04',
     tileSize: 512,
-    subdomains: "abc",
+    subdomains: 'abc',
     noWrap: true,
     continuousWorld: true,
     // Prevent Leaflet from retrieving non-existent tiles on the
@@ -73,32 +72,31 @@ window.onload = function () {
       [89.9999, 179.9999]
     ],
     attribution:
-      "<a href='https://wiki.earthdata.nasa.gov/display/GIBS'>" +
-      "NASA EOSDIS GIBS</a>&nbsp;&nbsp;&nbsp;" +
-      "<a href='https://github.com/nasa-gibs/web-examples/blob/release/examples/leaflet/geographic-epsg4326.js'>" +
-      "View Source" +
-      "</a>"
+      '<a href="https://wiki.earthdata.nasa.gov/display/GIBS">' +
+      'NASA EOSDIS GIBS</a>&nbsp;&nbsp;&nbsp;' +
+      '<a href="https://github.com/nasa-gibs/web-examples/blob/release/examples/leaflet/geographic-epsg4326.js">' +
+      'View Source' +
+      '</a>'
   });
 
   map.addLayer(layer);
 };
 
- /*
- * Workaround for 1px lines appearing in some browsers due to fractional transforms
- * and resulting anti-aliasing.
- * https://github.com/Leaflet/Leaflet/issues/3575
- */
-(function(){
-  var originalInitTile = L.GridLayer.prototype._initTile
+/*
+* Workaround for 1px lines appearing in some browsers due to fractional transforms
+* and resulting anti-aliasing.
+* https://github.com/Leaflet/Leaflet/issues/3575
+*/
+(function () {
+  var originalInitTile = L.GridLayer.prototype._initTile;
   L.GridLayer.include({
-      _initTile: function (tile) {
-          originalInitTile.call(this, tile);
+    _initTile: function (tile) {
+      originalInitTile.call(this, tile);
 
-          var tileSize = this.getTileSize();
+      var tileSize = this.getTileSize();
 
-          tile.style.width = tileSize.x + 1 + 'px';
-          tile.style.height = tileSize.y + 1 + 'px';
-      }
+      tile.style.width = tileSize.x + 1 + 'px';
+      tile.style.height = tileSize.y + 1 + 'px';
+    }
   });
-})()
-
+})();
