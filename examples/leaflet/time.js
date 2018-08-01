300df6d9af5bb0f0937a083295ea62e7dd4c8ddf
Mike McGann <mike.mcgann@nasa.gov>
2018-08-01 09:47:21 -0400

    fix tile seams leaflet time example

--- examples/leaflet/time.js
+++ examples/leaflet/time.js
@@ -130,3 +130,22 @@ window.onload = function () {
       update();
     });
 };
+
+/*
+* Workaround for 1px lines appearing in some browsers due to fractional transforms
+* and resulting anti-aliasing.
+* https://github.com/Leaflet/Leaflet/issues/3575
+*/
+(function () {
+  var originalInitTile = L.GridLayer.prototype._initTile;
+  L.GridLayer.include({
+    _initTile: function (tile) {
+      originalInitTile.call(this, tile);
+
+      var tileSize = this.getTileSize();
+
+      tile.style.width = tileSize.x + 1 + 'px';
+      tile.style.height = tileSize.y + 1 + 'px';
+    }
+  });
+})();
